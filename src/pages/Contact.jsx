c3fdd3d9555cb16ca0d7e3c037a108456f126c09
santhoshmsi2005 <santhoshmsi2005@gmail.com>
2026-07-19 21:03:42 +0530

    use react hook form with email message send

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,24 +7,54 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FiSend } from "react-icons/fi";
 import { motion } from 'framer-motion';
-import { fadeUp, fadeDown, fadeRight, fadeLeft, container } from '../components/animations/motionVariants';
+import { fadeUp, fadeRight, fadeLeft, container } from '../components/animations/motionVariants';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod'; // or 'zod/v4'
 
 const Contact = () => {
+    const schema = z.object({
+        name: z.string().min(1, { message: 'Required' }),
+        email: z.email({ message: 'Invalid email address' }),
+        message: z.string().min(1, { message: 'Required' }),
+    });
 
-    const [result, setResult] = useState("");
+    const { register, handleSubmit, reset, formState: { errors } } = useForm({ resolver: zodResolver(schema) })
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
+    const [result, setResult] = useState("")
+
+    // const handleSubmit = async (event) => {
+    //     event.preventDefault();
+    //     const formData = new FormData(event.target);
+    //     formData.append("access_key", "1f5868ce-fe82-4da4-be6f-1c357c8bbe87");
+
+    //     const response = await fetch("https://api.web3forms.com/submit", {
+    //         method: "POST",
+    //         body: formData
+    //     });
+
+    //     const data = await response.json();
+    //     setResult(data.success ? "Success!" : "Error");
+    // };
+
+    const HandleSubmitData = async (data) => {
+        setResult("Sending...");
+
+        const formData = new FormData();
+
         formData.append("access_key", "1f5868ce-fe82-4da4-be6f-1c357c8bbe87");
+        formData.append("name", data.name);
+        formData.append("email", data.email);
+        formData.append("message", data.message);
 
         const response = await fetch("https://api.web3forms.com/submit", {
             method: "POST",
-            body: formData
+            body: formData,
         });
 
-        const data = await response.json();
-        setResult(data.success ? "Success!" : "Error");
+        const result = await response.json();
+        setResult(result.success ? "Message sent successfully!" : result.message);
+        reset();
     };
 
     return (
@@ -100,21 +130,24 @@ const Contact = () => {
                         </motion.div>
 
                         {/* Right Side */}
-                        <motion.form variants={fadeRight(0.5, 0.7)} onSubmit={handleSubmit} className='flex flex-col p-10 justify-center w-full' >
+                        <motion.form variants={fadeRight(0.5, 0.7)} onSubmit={handleSubmit(HandleSubmitData)} className='flex flex-col p-10 justify-center w-full' >
                             <div className='border border-muted/50 p-10 rounded-2xl w-full h-full bg-surface/50 flex flex-col gap-4 shadow-[0_0_30px_8px_rgba(0,0,0,1)]'>
                                 <div className='flex flex-col gap-2'>
                                     <h1 className='text-sm text-muted font-medium'>Full Name</h1>
-                                    <input type="text" name='name' placeholder='Your Name' required className='border border-muted/50 w-full px-3 py-3 rounded-lg bg-background focus:outline-none focus:border-primary' />
+                                    <input {...register("name")} type="text" name='name' placeholder='Your Name' className='border border-muted/50 w-full px-3 py-3 rounded-lg bg-background focus:outline-none focus:border-primary' />
+                                    {errors.name?.message && <p className="text-red-500 text-sm">{errors.name.message}</p>}
                                 </div>
 
                                 <div className='flex flex-col gap-2'>
                                     <h1 className='text-sm text-muted font-medium'>Email Address</h1>
-                                    <input type="email" name='email' placeholder='Your Email Address' required className='border border-muted/50 w-full px-3 py-3 rounded-lg bg-background focus:outline-none focus:border-primary' />
+                                    <input {...register("email")} type="email" name='email' placeholder='Your Email Address' className='border border-muted/50 w-full px-3 py-3 rounded-lg bg-background focus:outline-none focus:border-primary' />
+                                    {errors.email?.message && <span className="text-red-500 text-sm">{errors.email.message}</span>}
                                 </div>
 
                                 <div className='flex flex-col gap-2'>
                                     <h1 className='text-sm text-muted font-medium'>Message</h1>
-                                    <textarea name='message' placeholder='Hi Santhosh, I have a project...' required className='border border-muted/50 w-full px-2 py-2 rounded-lg bg-background h-56 focus:outline-none focus:border-primary' />
+                                    <textarea {...register("message")} name='message' placeholder='Hi Santhosh, I have a project...' className='border border-muted/50 w-full px-2 py-2 rounded-lg bg-background h-56 focus:outline-none focus:border-primary' />
+                                    {errors.message?.message && <span className="text-red-500 text-sm">{errors.message.message}</span>}
                                 </div>
 
                                 <button type='submit' className='flex items-center justify-center gap-4 border text-primary border-primary p-3 rounded-xl bg-primary/10 cursor-pointer transition-all duration-300 hover:bg-primary hover:text-background hover:shadow-[0_0_7px_5px_var(--primary)/30]'>
